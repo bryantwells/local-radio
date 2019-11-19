7397ca9c579ea7fdb21810c0ad086f039a905efb
Bryant Wells <bryantwells@me.com>
2019-11-19 17:38:24 -0500

    push to share

--- public/js/socket.js
+++
@@ -1,7 +0,0 @@
-// Create WebSocket connection.
-const socket = new WebSocket('wss://localhost:3000');
-
-// Connection opened
-socket.addEventListener('open', function (event) {
-    socket.send('Hello Server!');
-});
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@ install icecast with ssl
 change ssl-certificate in icecast.xml
 
 ```
-icecast -c icecast.xml || icecast -c icecast.xml
+icecast -c icecast.xml
 ```
 ```
 npm run start
